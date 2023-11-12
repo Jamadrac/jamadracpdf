5aa0ae1cb22d3eed54ea9ae3d89c79b15a69d3ee
jamadrac <rabbikindalo001@gmail.com>
2023-11-12 11:33:37 +0200

    aliente bros

---
+++ android/app/src/main/kotlin/com/example/jamadracpdf/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.jamadracpdf
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/home_Page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "PDF viewer",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.red,
+      ),
+    );
+  }
+}
